c1e62ffff775c56cbf9c9cdd014aa35be7dd34a1
KYZX-PC\Kyzx <kyzx@bupt.edu.cn>
2017-05-09 16:47:47 +0800

    add some source file

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include "HelloWorld.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello, world!!" << endl;
+	return 0;
+}
---
+++ HelloWorld/HelloWorld/HelloWorld.h
@@ -0,0 +1,7 @@
+#ifndef _HELLOWORLD_H
+#define _HELLOWORLD_H
+
+#include <iostream>
+
+#endif // !_HELLOWORLD_H
+
